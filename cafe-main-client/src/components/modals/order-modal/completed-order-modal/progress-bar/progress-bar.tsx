5b7288e6ea1675c54c5aadde3758954b6d9b7bcf
sayonara213 <79828791+sayonara213@users.noreply.github.com>
2023-03-29 15:31:12 +0300

    progress-bar init

---
+++ cafe-main-client/src/components/modals/order-modal/completed-order-modal/progress-bar/progress-bar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface ProgressBarProps {
+  progress: number;
+  time: string;
+}
+
+const ProgressBar: React.FC<ProgressBarProps> = ({ progress, time }) => {
+  return (
+    <>
+      <></>
+    </>
+  );
+};
+
+export default ProgressBar;
